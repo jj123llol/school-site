7a82b2a5b703afab17ab2eb5c38c8aea3390510b
JJJ <115508192+jj123llol@users.noreply.github.com>
2024-09-16 14:15:05 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 document.getElementById('EaglerCraft 1.8').onclick = function() {
     window.open('https://jj123llol.github.io/school-site/minecraft%201.8.html', '_blank');
 }
+document.getElementById('EaglerCraft 1.5').onclick = function() {
+    window.open('https://jj123llol.github.io/school-site/minecraft%201.5.2.html', '_blank');
+}
