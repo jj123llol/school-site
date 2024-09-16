c93f790298c3c1abb97d48044d6830ca647798d7
JJJ <115508192+jj123llol@users.noreply.github.com>
2024-09-16 18:05:53 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
-window.history.pushState(“object or string”, “Home - Google Drive”, “hidden”);  
-
-document.getElementById('EaglerCraft 1.8').onclick = function() {
-    window.open('https://jj123llol.github.io/school-site/Eaglercraft%201.8%20offline.html', '_blank');
-}
-document.getElementById('EaglerCraft 1.5').onclick = function() {
-    window.open('https://jj123llol.github.io/school-site/minecraft%201.5.2.html', '_blank');
-}
-document.getElementById('UwUClient').onclick = function() {
-    window.open('https://file.io/LH7KvmOYouZT', '_blank');
-}
-document.getElementById('impquiz').onclick = function() {
-    window.open('https://impossible-quiz-zeta.vercel.app/', '_blank');
-}
-document.getElementById('impquiz2').onclick = function() {
-    window.open('https://impossible-quiz-e.vercel.app/', '_blank');
-}
-document.getElementById('impquiz3').onclick = function() {
-    window.open('https://impossible-quiz-e-git-main-broplsss.vercel.app/', '_blank');
-}
-document.getElementById('unfair').onclick = function() {
-    window.open('https://unfair-mario-eeic.vercel.app/', '_blank');
+if (window.confirm("This Is By Jude And Ian")){
+    document.getElementById('EaglerCraft 1.8').onclick = function() {
+        window.open('https://jj123llol.github.io/school-site/Eaglercraft%201.8%20offline.html', '_blank');
+    }
+    document.getElementById('EaglerCraft 1.5').onclick = function() {
+        window.open('https://jj123llol.github.io/school-site/minecraft%201.5.2.html', '_blank');
+    }
+    document.getElementById('UwUClient').onclick = function() {
+        window.open('https://file.io/LH7KvmOYouZT', '_blank');
+    }
+    document.getElementById('impquiz').onclick = function() {
+        window.open('https://impossible-quiz-zeta.vercel.app/', '_blank');
+    }
+    document.getElementById('impquiz2').onclick = function() {
+        window.open('https://impossible-quiz-e.vercel.app/', '_blank');
+    }
+    document.getElementById('impquiz3').onclick = function() {
+        window.open('https://impossible-quiz-e-git-main-broplsss.vercel.app/', '_blank');
+    }
+    document.getElementById('unfair').onclick = function() {
+        window.open('https://unfair-mario-eeic.vercel.app/', '_blank');
+    }
+} else{
+    window.history.back()
 }
