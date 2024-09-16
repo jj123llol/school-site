c59ed070ef5000a1c3ce49f5a5bf94cf2e64b578
JJJ <115508192+jj123llol@users.noreply.github.com>
2024-09-16 18:54:15 -0500

    Update index.js

--- index.js
+++ index.js
@@ -21,11 +21,8 @@ if (window.prompt("Enter Password: ","") == "Pizzarolls123") {
         window.open('https://unfair-mario-eeic.vercel.app/', '_blank');
     }
 } else{
-    var saved = document.getElementById('sch');
     while (document.body.firstChild) {
         document.body.removeChild(document.body.firstChild);
     }
     window.alert("Incorrect Password!")
-    document.body.appendChild(saved);
-    saved.text = "Wrong!"
 }
