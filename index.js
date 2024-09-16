9cec507bc0c6b80940fa5b4a4fa965f475e8e466
JJJ <115508192+jj123llol@users.noreply.github.com>
2024-09-16 15:07:52 -0500

    Update index.js

--- index.js
+++ index.js
@@ -4,3 +4,6 @@ document.getElementById('EaglerCraft 1.8').onclick = function() {
 document.getElementById('EaglerCraft 1.5').onclick = function() {
     window.open('https://jj123llol.github.io/school-site/minecraft%201.5.2.html', '_blank');
 }
+document.getElementById('UwuClient').onclick = function() {
+    window.open('https://file.io/LH7KvmOYouZT', '_blank');
+}
