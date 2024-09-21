5d2c77403150882f2dfcde8e431d8af49fc833d0
JJJ <115508192+jj123llol@users.noreply.github.com>
2024-09-21 14:41:51 -0500

    Update chat-room.js

--- chat-room.js
+++ chat-room.js
@@ -25,7 +25,7 @@ function createText(sentfrom, message, time){
 
 var username = prompt("Enter User: ","")
 
-if (username == null || username == "" || username.toLowerCase() == "jude"){
+if (username == null || username == "" || username.toLowerCase() == "jude" || username.toLowerCase() == "server"){
   username = randomstring(10)
 }
 
@@ -33,9 +33,15 @@ if (username == null || username == "" || username.toLowerCase() == "jude"){
 createText('Server','Welcome!',getTime())
 
 websocket.addEventListener("open", (event) => {
-  websocket.send(username + " Has Connected!");
+	message = getTime() + "dsfejvcjkdrgkjbhjdkjb" + "server" + "dsfejvcjkdrgkjbhjdkjb" + username + " Has Connected!"
+  websocket.send(message)
 });
 
 websocket.addEventListener("message", (event) => {
-  createText("Message from server ", event.data);
+	received = String(event.data)
+  split = received.split("dsfejvcjkdrgkjbhjdkjb")
+  timesent = split[0]
+  sentby = split[1]
+  message = split[2]
+  createText(sentby, message, timesent)
 });
