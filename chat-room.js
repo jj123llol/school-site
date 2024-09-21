bdc4f64fe49560af6eede3c7ded89d2c17c2e39c
JJJ <115508192+jj123llol@users.noreply.github.com>
2024-09-21 14:00:54 -0500

    Update chat-room.js

--- chat-room.js
+++ chat-room.js
@@ -1,3 +1,15 @@
+var alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+
+function randomstring(letters) {
+  var indx = 0
+  var string = ""
+  while (indx < letters) {
+    string = string + alphabet[(Math.floor(Math.random() * alphabet.length))]
+    indx = indx + 1
+  }
+  return string
+}
+
 function getTime(){
   today = new Date()
   var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds()
@@ -9,4 +21,9 @@ function createText(sentfrom, message, time){
   document.body.appendChild(message)
 }
 
+var name = prompt("Enter User: ","")
+
+if (name == null || name == ""){
+  name = randomstring(10)
+}
 createText('Server','Welcome!',getTime())
